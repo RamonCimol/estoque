e7970a9a3f2d4ce23897526c0b5a3dcdfcd1eb0e
Alisson Adriano Freitag <alisson.a.freitag@gmail.com>
2025-10-20 00:51:07 -0300

    alterações na estrutura do projeto arquitetura API camadas

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
-import path from 'path';
-import authRoutes from './routes/authRoutes'; // Importa as rotas de autenticação
+import { pool } from './database'; // Importe o pool de conexões com o MySQL
+import authRoutes from './routes/registerRoutes'; // Importa as rotas de autenticação
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware para processar requisições JSON
 app.use(express.json());
-
-// Servir os ficheiros estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Usa as rotas de autenticação, prefixando-as com '/api/auth'
 app.use('/api/auth', authRoutes);
